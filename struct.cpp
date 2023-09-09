669ed1ef947dee4c71827810f38cc781928292df
Shivam Kumar <skshivam466@gmail.com>
2023-09-09 10:36:48 +0545

    boiler

--- struct.cpp
+++ struct.cpp
@@ -7,6 +7,7 @@ struct student{
 // yet to be completed
 int main(){
     cin>>s1.roll>>s1.section;
-    cout<<sizeof(s1.roll)<<sizeof(s1.section);
+    cout<<sizeof(s1.roll)<<endl<<sizeof(s1.section)<<endl;
+    cout<<s1.roll<<endl<<s1.section;
     return 0;
 }
