a0a1dca75c5ef380e99ea786cf461ee20be6ee32
Shivam Kumar <skshivam466@gmail.com>
2023-09-09 10:32:50 +0545

    struct boiler

---
+++ struct.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+struct student{
+    int roll;
+    char section;
+}s1,s2;
+// yet to be completed
+int main(){
+    cin>>s1.roll>>s1.section;
+    cout<<sizeof(s1.roll)<<sizeof(s1.section);
+    return 0;
+}
